c8e849e2702ed7f02777b5622023f17f82d01a7d
Gabriel Lantin <mushymanee@gmail.com>
2021-11-15 09:16:48 -0800

    server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger('dev'))
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+app.use(require("./routes/api"))
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+})
